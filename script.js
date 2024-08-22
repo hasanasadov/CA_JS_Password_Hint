aafdd034a28b34fdc0d6db98821abd88f3b8b364
hasanasadov <hasanaliasadov@gmail.com>
2024-08-23 01:54:26 +0400

    symbol error handled

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const input = document.querySelector('input');
         let value = e.target.value.trim();
         value.length >= 8 ? hints.querySelector('span:nth-child(1)').classList.add('green') : hints.querySelector('span:nth-child(1)').classList.remove('green');
         value.match(/[0-9]/) ? hints.querySelector('span:nth-child(2)').classList.add('green') : hints.querySelector('span:nth-child(2)').classList.remove('green');
-        value.match(/[!@#$%^&*-_./?]/) ? hints.querySelector('span:nth-child(3)').classList.add('green') : hints.querySelector('span:nth-child(3)').classList.remove('green');
+        value.match(/[!@#$%^&\*\-_\.\\,`~()+=<>"':;\[\]{}/?]/) ? hints.querySelector('span:nth-child(3)').classList.add('green') : hints.querySelector('span:nth-child(3)').classList.remove('green');
         value.match(/[A-Z]/) ? hints.querySelector('span:nth-child(4)').classList.add('green') : hints.querySelector('span:nth-child(4)').classList.remove('green');
         value.match(/[a-z]/) ? hints.querySelector('span:nth-child(5)').classList.add('green') : hints.querySelector('span:nth-child(5)').classList.remove('green');
     })
